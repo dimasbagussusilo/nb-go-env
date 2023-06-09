a49ec25a883ef816825f62106c02a32c66bc60a1
Dimas Bagus Susilo <dimasbagussusilo@Dimass-MacBook-Pro.local>
2023-06-09 19:28:02 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Noob-Env
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/dimasbagussusilo/nb-go-env.svg)](https://pkg.go.dev/github.com/dimasbagussusilo/nb-go-env)
+![GitHub go.mod Go version (subdirectory of monorepo)](https://img.shields.io/github/go-mod/go-version/dimasbagussusilo/nb-go-env?style=flat-square)
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/dimasbagussusilo/nb-go-env?style=flat-square)
+
+Noob Env is a tools for working with `.env`, such as:
+- Load Env
+- Get Env
+
+Noob Env use [GoDotEnv v1.4.0](github.com/joho/godotenv) to load & parse `.env`. Thanks to [*godotenv*](github.com/joho/godotenv)**
+
+## Contents
+
+- [Noob-Env](#Noob-Env)
+  - [Installation](#Installation)
+  - [Quick Start & Usage](#Quick)
+
+## Installation
+
+To install this package, you need to install Go (**version 1.17+ is required**) & initiate your Go workspace first.
+
+1. After you initiate your workspace then you can install this package with below command.
+
+```shell
+go get -u github.com/dimasbagussusilo/nb-go-env
+```
+
+2. Import it in your code
+
+```go
+import "github.com/dimasbagussusilo/nb-go-env"
+```
+
+## Quick Start & Usage
+
+See the test:
+- [env_test.go](env_test.go)
+
+## Contributors ##
+
+- Alfarih Faza <alfarihfz@gmail.com>
+
+## License
+
+This project is licensed under the - see the [LICENSE.md](LICENSE.md) file for details
---
+++ env.go
@@ -0,0 +1,261 @@
+package env
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/dimasbagussusilo/nb-go-parser"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"reflect"
+)
+
+type Err struct {
+	e error
+}
+
+// HasZeroValue Check a variable has Zero Value
+func hasZeroValue(v interface{}) bool {
+	if v == nil {
+		return true
+	}
+
+	t := reflect.TypeOf(v)
+	if t == nil {
+		return true
+	}
+
+	switch t.Kind() {
+	case reflect.Map, reflect.Slice, reflect.Array:
+		return false
+	}
+
+	return v == reflect.Zero(t).Interface()
+}
+
+func (e *Err) Errorf(f string, s ...interface{}) *Err {
+	return &Err{
+		e: errors.New(fmt.Sprintf(fmt.Sprintf("this %s: %s", e.e.Error(), f), s...)),
+	}
+}
+
+func (e *Err) Error() string {
+	return e.e.Error()
+}
+
+func NewEnvErr(s string) *Err {
+	return &Err{
+		e: errors.New(s),
+	}
+}
+
+var ErrorVarNotExist = NewEnvErr("variable not exist")
+
+type env struct {
+	envs      map[string]string
+	useDotEnv bool
+}
+
+type Env interface {
+	GetInt(k string, def ...int) (int, error)
+	MustGetInt(k string, def ...int) int
+	GetString(k string, def ...string) (string, error)
+	MustGetString(k string, def ...string) string
+	GetBool(k string, def ...bool) (bool, error)
+	MustGetBool(k string, def ...bool) bool
+	GetStringArr(k string, def ...[]string) ([]string, error)
+	MustGetStringArr(k string, def ...[]string) []string
+	GetIntArr(k string, def ...[]int) ([]int, error)
+	MustGetIntArr(k string, def ...[]int) []int
+	Dump() (string, error)
+}
+
+func (c env) GetInt(k string, def ...int) (int, error) {
+	cfg, exist := c.get(k)
+	if !exist {
+		if len(def) == 0 {
+			return 0, ErrorVarNotExist.Errorf("%s", k)
+		}
+		return def[0], nil
+	}
+
+	i, e := parser.String(cfg).ToInt()
+
+	if e != nil {
+		if len(def) == 0 {
+			return 0, e
+		}
+
+		return def[0], nil
+	}
+
+	return i, e
+}
+
+func (c env) MustGetInt(k string, def ...int) int {
+	v, err := c.GetInt(k, def...)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c env) GetString(k string, def ...string) (string, error) {
+	cfg, exist := c.get(k)
+	if !exist {
+		if len(def) == 0 {
+			return "", ErrorVarNotExist.Errorf("%s", k)
+		}
+		return def[0], nil
+	}
+
+	return cfg, nil
+}
+
+func (c env) MustGetString(k string, def ...string) string {
+	v, err := c.GetString(k, def...)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c env) GetBool(k string, def ...bool) (bool, error) {
+	cfg, exist := c.get(k)
+	if !exist {
+		if len(def) == 0 {
+			return false, ErrorVarNotExist.Errorf("%s", k)
+		}
+		return def[0], nil
+	}
+
+	b, e := parser.String(cfg).ToBool()
+	if e != nil {
+		if len(def) == 0 {
+			return false, e
+		}
+
+		return def[0], nil
+	}
+
+	return b, e
+}
+
+func (c env) MustGetBool(k string, def ...bool) bool {
+	v, err := c.GetBool(k, def...)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c env) get(k string) (string, bool) {
+	if c.useDotEnv {
+		cfg, exist := c.envs[k]
+
+		return cfg, !hasZeroValue(cfg) && exist
+	}
+
+	cfg := os.Getenv(k)
+	return cfg, !hasZeroValue(cfg)
+}
+
+func (c env) GetStringArr(k string, def ...[]string) ([]string, error) {
+	cfg, exist := c.get(k)
+	if !exist {
+		if len(def) == 0 {
+			return nil, ErrorVarNotExist.Errorf("%s", k)
+		}
+
+		return def[0], nil
+	}
+
+	return parser.String(cfg).ToStringArr()
+}
+
+func (c env) MustGetStringArr(k string, def ...[]string) []string {
+	v, err := c.GetStringArr(k, def...)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c env) GetIntArr(k string, def ...[]int) ([]int, error) {
+	cfg, exist := c.get(k)
+	if !exist {
+		if len(def) == 0 {
+			return nil, ErrorVarNotExist.Errorf("%s", k)
+		}
+		return def[0], nil
+	}
+
+	is, e := parser.String(cfg).ToIntArr()
+
+	if e != nil {
+		if len(def) == 0 {
+			return nil, e
+		}
+
+		return def[0], nil
+	}
+
+	return is, e
+}
+
+func (c env) MustGetIntArr(k string, def ...[]int) []int {
+	v, err := c.GetIntArr(k, def...)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func (c env) Dump() (string, error) {
+	if !c.useDotEnv {
+		return "", errors.New("cannot dump env, you are using system-wide env")
+	}
+
+	j, e := json.Marshal(c.envs)
+
+	return string(j), e
+}
+
+func LoadEnv(envPath string, fallbackToWide ...bool) (Env, error) {
+	fBackToWide := false
+	if len(fallbackToWide) > 0 {
+		fBackToWide = fallbackToWide[0]
+	}
+
+	envs, err := godotenv.Read(envPath)
+	if err != nil && !fBackToWide {
+		return nil, err
+	}
+
+	if err != nil {
+		if !fBackToWide {
+			return nil, err
+		}
+
+		log.Printf("%s \n %s", "Warning! You are use System Wide Environment due to this error:", err.Error())
+		return env{
+			envs:      envs,
+			useDotEnv: false,
+		}, nil
+	}
+
+	for key, val := range envs {
+		err = os.Setenv(key, val)
+	}
+
+	return env{
+		envs:      envs,
+		useDotEnv: err == nil,
+	}, nil
+}
